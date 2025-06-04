e42b512e40f163b8bdd9136be9796d05dfc1cdbb
Tafil Suhagerllaj <t.suhagerllaj@gmail.com>
2025-06-04 22:13:39 +0200

    Second batch fix

--- admin/config/general.php
+++ admin/config/general.php
@@ -4,7 +4,7 @@ $app_name = 'VoltKos CRM';
 $logo = './static/VoltKosLogoTransBack.png';
 $logo_admin = './static/VoltKosLogoTransBackWhite.png';
 
-$app_version = 'v0.1.0-mvp';
+$app_version = 'v0.1.2-mvp';
 $app_copyright_date =  date("Y");
 
 
